2bde873bdb3dd03238ff86acab16bfa51b106932
Samantha Samuels <ssamuels327@gmail.com>
2025-02-16 18:56:37 -0500

    Final changes

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 '''
 # Routing for your application.
 # Put your routes below this comment
@@ -18,3 +19,6 @@ def about():
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+if __name__ == '__main__':
+    app.run(debug=True)
